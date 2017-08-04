ba5780c9e123124eaf2dc5f075e07a1ef9684e7c
Dennis Rhee <idrhee@MacBook.local>
2017-08-04 08:44:13 +0300

    disqus..

--- blogdown_serve.R
+++ blogdown_serve.R
@@ -1,8 +1,8 @@
 
-setwd("/Users/idrhee/Dropbox/_doc/_archive/other/my_website/")
+#setwd("/Users/idrhee/Dropbox/_doc/_archive/other/my_website/")
 library(blogdown)
 
 #new_content("/post/kenya2017.rmd")
 
-hugo_build()
+#hugo_build()
 serve_site()
